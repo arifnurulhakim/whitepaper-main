1df35aca4c5ef6dc59c0aab64ad8ae729fcc4e23
arifnurulhakim <arifnhakim23@gmail.com>
2023-12-01 13:10:25 +0700

    update db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  HOST: "tiny.db.elephantsql.com",
-  USER: "rwygbrux",
-  PASSWORD: "3ECCf24BLY5q6P76CsebrfRwJDbQ5VeX",
-  DB: "rwygbrux",
+  // HOST: "tiny.db.elephantsql.com",
+  // USER: "rwygbrux",
+  // PASSWORD: "3ECCf24BLY5q6P76CsebrfRwJDbQ5VeX",
+  // DB: "rwygbrux",
+  // dialect: "postgres",
+  HOST: "user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com",
+  USER: "whitepapper-main-db-0acc8b69bc342c60d",
+  PASSWORD: "Htds5BrNk7cCGnnmyxUp31KyvwrtEX",
+  DB: "whitepapper-main-db-0acc8b69bc342c60d",
   dialect: "postgres",
   dialectOptions: {
     idle_in_transaction_session_timeout: 1000,
