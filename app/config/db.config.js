f89d8d342acab6f19dadb94c443b01e02af9b28a
arifnurulhakim <arifnhakim23@gmail.com>
2023-12-01 12:19:39 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,10 @@ module.exports = {
   PASSWORD: "3ECCf24BLY5q6P76CsebrfRwJDbQ5VeX",
   DB: "rwygbrux",
   dialect: "postgres",
+  dialectOptions: {
+    idle_in_transaction_session_timeout: 1000,
+    
+  },
   pool: {
     max: 5,
     min: 0,
