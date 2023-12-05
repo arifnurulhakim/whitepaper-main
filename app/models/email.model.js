f665429ec14346204353469a0afb8a5064189593
arifnurulhakim <60438641+arifnurulhakim@users.noreply.github.com>
2023-12-05 12:16:26 +0700

    changes validation model

--- app/models/email.model.js
+++ app/models/email.model.js
@@ -2,7 +2,15 @@ module.exports = (sequelize, Sequelize) => {
     const Email = sequelize.define("email", {
         email: {
             type: Sequelize.STRING,
-            unique: false
+            unique: false,
+            allowNull: false,
+            unique: false,
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: 'Invalid email format. Please provide a valid email address.',
+                },
+            },
         }
     });
     return Email;
